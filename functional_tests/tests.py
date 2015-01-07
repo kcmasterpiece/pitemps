176549111e8d6796ab1e7b7db7f88bf024aaa693
Jeff Fontas <fontas.j+github@gmail.com>
2015-01-07 17:44:14 -0500

    added bootstrap, turned off admin

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,15 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from tempGraph.models import Readings
+from datetime import datetime
+import re
 import unittest
 
 class NewVisitorTest(StaticLiveServerTestCase):
 
 	def setUp(self):
+		newReading = Readings.objects.create(temp=99)
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
 
@@ -20,4 +24,15 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		# Observe page title and header elements
 		self.assertIn('Current Room Temps', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
-		self.assertRegex(header_text,'$Current Room Temperature: \d+°^')
+		self.assertRegex(header_text,r"^Current Room Temperature: \d+°$")
+
+		
+	def test_layout_and_styling(self):
+		# Open home page
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024,768)
+		# check centered header
+		header = self.browser.find_element_by_tag_name('h1')
+		header_text_alignment = header.value_of_css_property("text-align")
+		self.assertEqual(header_text_alignment, 'center')
+
--- tempGraph/models.py
+++ tempGraph/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 class Readings(models.Model):
 	temp = models.DecimalField(decimal_places=2,max_digits=5, default=0)
+	time = models.DateTimeField(default=datetime.now)
+
+	class Meta:
+		get_latest_by = 'time'
