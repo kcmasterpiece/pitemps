76ac08bc2dd06c50bd22c623e4cf6088f655c753
Jeff <fontas.j@gmail.com>
2015-01-07 20:48:42 -0500

    added functional test hack for staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,10 +3,24 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from tempGraph.models import Readings
 from datetime import datetime
-import re
+import sys
 import unittest
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		for arg in sys.argv:
+			if 'liveserver' in arg:
+				cls.server_url = 'http://' + arg.split('=')[1]
+				return
+		super().setUpClass()
+		cls.server_url = cls.live_server_url
+
+	@classmethod
+	def tearDownClass(cls):
+		if cls.server_url == cls.server_url:
+			super().tearDownClass()
 
 	def setUp(self):
 		newReading = Readings.objects.create(temp=99)
@@ -19,7 +33,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 	def test_can_open_website(self):
 
 		# Open the website
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 
 		# Observe page title and header elements
 		self.assertIn('Current Room Temps', self.browser.title)
@@ -29,7 +43,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 		
 	def test_layout_and_styling(self):
 		# Open home page
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.server_url)
 		self.browser.set_window_size(1024,768)
 		# check centered header
 		header = self.browser.find_element_by_tag_name('h1')
