1bdc80269a656e9ed238f091b389062b6ba04b64
Jeff Fontas <fontas.j+github@gmail.com>
2015-05-30 15:01:54 -0400

    added humidity to db, modified dht22.py to create records in db

--- tempGraph/models.py
+++ tempGraph/models.py
@@ -4,7 +4,9 @@ from django.utils import timezone
 
 # Create your models here.
 class Readings(models.Model):
+	
 	temp = models.DecimalField(decimal_places=2,max_digits=5, default=0)
+	humidity = models.DecimalField(decimal_places=2, max_digits=5, default=0)
 	time = models.DateTimeField(default=timezone.now)
 
 	class Meta:
